06d0f86c9b5773281196de23a764a7711b5543fd
Florian Jourde <jourdeflorian@gmail.com>
2021-11-09 04:03:16 +0100

    Submitting solution

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// var img = document.getElementsByTagName("svg")[0];
-// var bbox = svg.getBBox();
-// var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
-// svg.setAttribute("viewBox", viewBox);
-// prompt("Copy to clipboard: Ctrl+C, Enter", svg.outerHTML);
+let burgerIcon = document.getElementById('burger-icon');
+let burgerMenu = document.getElementById('burger-menu');
+let details = document.querySelectorAll('details');
+
+burgerIcon.addEventListener('click', function() {
+  burgerMenu.classList.toggle('active');
+})
+
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+
+window.addEventListener('resize', function() {
+  if (window.matchMedia("(min-width: 600px)").matches) {
+    burgerMenu.classList.remove('active');
+  }
+})
