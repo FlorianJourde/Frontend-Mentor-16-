3d63b04f51fbc3abac35d252fbdb5de330e67528
Florian Jourde <jourdeflorian@gmail.com>
2021-11-11 02:08:24 +0100

    Updating burger-icon animation

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ let details = document.querySelectorAll('details');
 burgerIcon.addEventListener('click', function() {
   burgerMenu.classList.toggle('active');
   burgerIcon.children[0].style.opacity = 0;
+  burgerIcon.children[0].style.marginLeft = -1 + "em";
+  burgerIcon.children[0].style.marginRight = 1 + "em";
 
   if (burgerMenu.classList.contains('active')) {
     burgerIcon.children[0].addEventListener('webkitTransitionEnd', function() {
       burgerIcon.children[0].src = "images/icon-close.svg"
       burgerIcon.children[0].style.opacity = 1
+      burgerIcon.children[0].style.marginLeft = 0 + "em";
+      burgerIcon.children[0].style.marginRight = 0 + "em";
     });
   } else {
     burgerIcon.children[0].addEventListener('webkitTransitionEnd', function() {
       burgerIcon.children[0].src = "images/icon-hamburger.svg";
       burgerIcon.children[0].style.opacity = 1
+      burgerIcon.children[0].style.marginLeft = 0 + "em";
+      burgerIcon.children[0].style.marginRight = 0 + "em";
     });
   }
 })
